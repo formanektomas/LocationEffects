fc86e7961e5afb8c549924ee6b0e0b2f7b86820f
Tomas Formanek <formanektomas@users.noreply.github.com>
2021-08-07 17:34:01 +0200

    Add files via upload

--- 6_CentralityIndices.R
+++ 6_CentralityIndices.R
@@ -91,3 +91,71 @@ Stores
 Stores$cc <- scale(Stores$cc)
 #
 Stores
+#
+#####################################################################
+# 
+# Illustrative plot - shows Closeness centrality heatmap  
+# 
+# activate map elemets (streets)
+edges <- net %>% 
+  st_as_sf("edges") %>% 
+  st_transform(4326)
+# activate map elemets (junctions)
+nodes <- net %>% 
+  st_as_sf("nodes") %>% 
+  st_transform(4326)
+nodes$cc <- scale(nodes$cc)[,1]
+# set map area
+bboxLiberec <- st_bbox(c(xmin = 15.045, xmax = 15.075, ymax = 50.78, ymin = 50.757), crs = st_crs(4326))
+bboxLiberec <- st_as_sfc(bboxLiberec)
+# remove elements outside plotted area (simplified data handling)
+LibCentNodes <- nodes %>% 
+  dplyr::select(cc) %>% 
+  sf::st_intersection(bboxLiberec)
+#
+LibCentEdges <- edges %>% 
+  dplyr::select(cc) %>% 
+  sf::st_intersection(bboxLiberec)
+#
+# Use ggmap package - you need to provide your own Google key
+# .. generous amount of free "queries" is provided (monthly)
+library(ggmap)
+register_google(key = "Your own Google key goes here") # follow ?register_google
+#
+#
+Lib_map <- get_map(c(left = 15.045, bottom = 50.757, right = 15.075, top = 50.78), 
+                   maptype = "terrain", crop = T, language = "en-EN",
+                   color="color")
+#
+ggmap(Lib_map)
+#
+# prepare heatmap (interpolate map elements with cc values over the whole map)
+#
+library(sp)
+# get the min/max range for lat/long to make an empty grid 
+x.range <- c(15.045,15.075) # min/max longitude of the interpolation area
+y.range <- c(50.757,50.78)  # min/max latitude of the interpolation area  
+# from the range, exapnd the coordinates to make a regular grid
+grd <- expand.grid(x = seq(from = x.range[1], to = x.range[2], by = 0.00025), 
+                   y = seq(from = y.range[1], to = y.range[2], by = 0.00025))
+coordinates(grd) <- ~x + y
+sp::gridded(grd) <- TRUE
+proj4string(grd) <- CRS("+init=epsg:4326") # convert to the epsg:4326
+#
+LB <- LibCentNodes %>% 
+  as("Spatial") 
+proj4string(LB) <- CRS("+init=epsg:4326") 
+# perform the interpolation of cc values
+idw <- gstat::idw(cc ~ 1, locations = LB, newdata = grd)  # apply idw model for the data
+idw.output = as.data.frame(idw)
+idw.output$Closeness <- idw.output$var1.pred
+# Plot2
+ggmap(Lib_map) +
+  geom_sf(data = LibCentEdges, inherit.aes = F, colour="darkgrey",size = 0.5)+
+  geom_tile(data = idw.output, aes(x = x, y = y, fill = Closeness), alpha = 0.45) + 
+  scale_fill_distiller(palette = "RdYlGn", direction = 1) 
+#
+#
+#
+#
+#
